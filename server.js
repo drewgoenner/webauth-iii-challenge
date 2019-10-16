68c00e67bff549167e56edb665d1c4767352d462
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-10-16 07:30:44 -0500

    set up dependencies, created index, server, knexfile, dbConfig, and file structure for afternoon project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const userRouter = require('./users/userRouter.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use('/api', userRouter);
+
+module.exports = server;
